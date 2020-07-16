508794b99b8ae6d4a867e9f82f6f375f657e94a6
samover <samuel.overloop@gmail.com>
2020-07-16 14:22:44 +0200

    Uses custom domain

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const slsw = require('serverless-webpack');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const isLocal = slsw.lib.webpack.isLocal;
 
@@ -10,6 +11,21 @@ module.exports = {
   mode: isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
   externals: [nodeExternals()],
+  optimization: {
+    minimize: false,
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        sourceMap: true,
+        parallel: true,
+        cache: true,
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: false,
+        },
+      }),
+    ],
+  },
   devtool: 'source-map',
   resolve: {
     extensions: [ '.js', '.jsx', '.json', '.ts', '.tsx' ],
